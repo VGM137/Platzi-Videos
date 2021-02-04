4a588feba17be4704eb0994486e4bd83a43f7f25
VGM137 <victorgestaltung@gmail.com>
2021-02-04 00:07:57 -0600

    Terminado el curso práctico de React

---
+++ src/containers/App.jsx
@@ -0,0 +1,54 @@
+import React, { useState, useEffect } from 'react'
+import Header from '../components/Header.jsx'
+import Search from '../components/Search.jsx'
+import Categories from '../components/Categories.jsx'
+import Carousel from '../components/Carousel.jsx'
+import CarouselItem from '../components/CarouselItem.jsx'
+import Footer from '../components/Footer.jsx'
+import useInitialState from '../hooks/useInitialState.js'
+import '../assets/styles/App.scss'
+import '../assets/styles/Media.scss'
+
+const API = 'http://localhost:3000/initalState/'
+
+const App = () => {
+  const initialState = useInitialState(API)
+  console.log(initialState)
+  return initialState.length === 0 ? <h1>Loading...</h1> : (
+    <div className='App'>
+      <Header />
+
+      <Search />
+      
+      {initialState.mylist.length > 0 &&
+        <Categories title='Mi lista'>
+          <Carousel>
+            {initialState.mylist.map(item => 
+            <CarouselItem  key={item.id}{...item}/>
+            )}
+          </Carousel>
+        </Categories>
+      }
+
+      <Categories title='Tendencias'>
+        <Carousel>
+          {initialState.trends.map(item => 
+          <CarouselItem  key={item.id}{...item}/>
+          )}
+        </Carousel>
+      </Categories>
+
+      <Categories title='Originals'>
+        <Carousel>
+          {initialState.originals.map(item => 
+          <CarouselItem  key={item.id}{...item}/>
+          )}
+        </Carousel>
+      </Categories>
+
+      <Footer />
+
+    </div>
+)}
+
+export default App
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import HelloWorld from './components/HelloWorld.jsx'
+import App from './containers/App.jsx'
+
 /* debugger */
-ReactDOM.render(<HelloWorld />, document.getElementById("app"))
+ReactDOM.render(<App />, document.getElementById("app"))
